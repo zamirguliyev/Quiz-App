9787421527f2628a581c8477df52044d9f275542
zamirguliyev <zamirquliyev7@gmail.com>
2022-06-14 14:36:24 +0400

    text

--- src/App.js
+++ src/App.js
@@ -1,24 +1,24 @@
-import logo from './logo.svg';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import './App.css';
+import Authenticate from './components/Authenticate';
+import Layout from './components/Layout';
+import Login from './components/Login';
+import Quiz from './components/Quiz';
+import Result from './components/Result';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Login />} />
+        <Route element={<Authenticate />}>
+          <Route path="/" element={<Layout />}>
+            <Route path="/quiz" element={<Quiz />} />
+            <Route path="/result" element={<Result />} />
+          </Route>
+        </Route>
+      </Routes>
+    </BrowserRouter >
   );
 }
 
